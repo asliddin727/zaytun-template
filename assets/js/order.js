3c7cff47d512e63cd62673f130476be0a05d8a8e
asliddin727 <asliddinmeyliyev1@gmail.com>
2024-03-04 11:53:31 +0500

    A non-empty order section

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,7 +1,11 @@
-// 'use strict'
+'use strict'
 
 const orderListActive = document.querySelectorAll('.order-menu .item'),
-    orderListCount = document.querySelectorAll('.order-menu .item .count')
+    orderListCount = document.querySelectorAll('.order-menu .item .count'),
+    orderEmptyContainer = document.querySelector('.my-order-container-empty'),
+    orderContainer = document.querySelector('.my-order-container'),
+    orderTypes = document.querySelector('.my-order .order-types'),
+    orderStatus = document.querySelectorAll('.order-item-header .status')
 
 orderListActive.forEach(item => {
     item.addEventListener('click', () => {
@@ -24,4 +28,24 @@ orderListCount.forEach(countItem => {
         countItem.style.backgroundColor = 'transparent';
     }
 });
+
+
+if(orderContainer.innerHTML == false){
+    orderContainer.style.display = 'none';
+    orderTypes.style.display = 'none';
+    orderEmptyContainer.style.display = 'flex';
+}
+
+
+orderStatus.forEach(item => {
+    if(item.innerText === 'Ожидается оплата'){
+        item.style.backgroundColor = '#F2994A';
+    }else if(item.innerText === 'Доставляется'){
+        item.style.backgroundColor = 'orange';
+    }else if(item.innerText === 'Обрабока заказа'){
+        item.style.backgroundColor = 'gray';
+    }
+})
+
+
 
