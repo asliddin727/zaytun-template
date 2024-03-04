9ad395aa53ab376ecfc353347fb1ad2ff7832348
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2024-03-04 21:34:52 +0500

    zaytun-hero

--- script/script.js
+++ script/script.js
@@ -3,3 +3,13 @@ function closeIcon(){
     const loginCloseIcon = document.getElementById("login");
     loginCloseIcon.style.display = "none";
 }
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+      perPage: 1,
+      rewind : true,
+      autoplay: true,
+    } );
+    
+    splide.mount();
+    } );
